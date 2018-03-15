edec3acfd391e22dd05bb2043f7101e22e9ae679
prateek <prateekdi1@gmail.com>
2018-03-15 18:58:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Aux from './hoc/Auxillary/Auxillary'
+
+class App extends Component {
+  render() {
+    return (
+      <Aux>
+
+        <p>Test App</p>
+      
+      
+      </Aux>
+    );
+  }
+}
+
+export default App;
